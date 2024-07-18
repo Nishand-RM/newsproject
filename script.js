2c5541cca1f58c6a5df2d1a2762f07d0a8f1f06a
Nishand-RM <nishandrm9@gmail.com>
2024-07-18 18:25:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,90 +1,88 @@
-function label_create(tagname,attrname,attrvalue,content ){
-        var ele = document.createElement(tagname);
-        ele.setAttribute(attrname,attrvalue);
-        ele.innerHTML = content;
-        return ele;
+.box{
+     background-color: rgb(199, 197,197);
+
 }
 
-function line_break(){
-    var ele = document.createElement("br");
-    return ele;
+.topcontainer{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: 15%;
 }
 
-function input_item(tagname,attrname,attrvalue,attrname1,attrvalue1){
-    var ele = document.createElement(tagname);
-    ele.setAttribute(attrname,attrvalue);
-    ele.setAttribute(attrname1,attrvalue1);
-    return ele;
+.one{
+    border-radius: 15px;
+    width: -5%;
+    height: 110px;
 }
 
-function button_tag(tagname,attrname,attrvalue,attrname1,attrvalue1,content){
-     var ele = document.createElement(tagname);
-     ele.setAttribute(attrname,attrvalue);
-    ele.setAttribute(attrname1,attrvalue1);
-    ele.innerHTML = content;
-    return ele;
+.three{
+    border-radius: 15px;
+    width: 19%;
+    height: 90px;
+    display: flex;
+    justify-content: flex-end;
+    
 }
 
 
-var first_label = label_create("label","for","first_name","First_Name");
-var break_br = line_break();
-var first_input =input_item ("input","type","text","id","first_name");
-document.body.append(first_label,break_br,first_input);
-
-var break_br = line_break();
-document.body.append(break_br);
-
-var middle_label = label_create("label","for","middle_name","Middle_Name");
-var break_br = line_break();
-var middle_input =input_item ("input","type","text","id","middle_name");
-document.body.append(middle_label,break_br,middle_input);
-
-var break_br = line_break();
-document.body.append(break_br);
-
-var last_label = label_create("label","for","last_name","Last_Name");
-var break_br = line_break();
-var last_input =input_item ("input","type","text","id","last_name");
-document.body.append(last_label,break_br,last_input);
-
-
-var break_br = line_break();
-document.body.append(break_br);
-
-var email_label = label_create("label","for","email","Email");
-var break_br = line_break();
-var email_input =input_item ("input","type","email","id","email");
-document.body.append(email_label,break_br,email_input);
-
-var break_br = line_break();
-document.body.append(break_br);
-
-var password_label = label_create("label","for","password","PASSWORD");
-var break_br = line_break();
-var password_input =input_item ("input","type","Password","id","password");
-document.body.append(password_label,break_br,password_input);
-
-var break_br = line_break();
-document.body.append(break_br);
-
-var button_input = button_tag("button","type","button","onclick","foo()","submit");
-var break_br = line_break();
-document.body.append(button_input,break_br);
-
-function foo(){
-    var res = document.getElementById("first_name").value;
-    console.log(res);
-
-    var res = document.getElementById("middle_name").value;
-    console.log(res);
-
-    var res = document.getElementById("last_name").value;
-    console.log(res);
-
-    var res = document.getElementById("email").value;
-    console.log(res);
-
-    var res = document.getElementById("password").value;
-    console.log(res);
+.flexbox1{
+    display: flex;
+    justify-content: space-between;
+    width: 45%;
 }
 
+.flex2{
+    position: relative;
+    right: -6px;
+}
+
+.flexbox2{
+    display: flex;
+    justify-content: space-between;
+    width: 45%;
+}
+
+.flex4{
+    position: relative;
+    right: -10px;
+}
+
+.container{
+    width: 33%;
+    height: 50%;
+    position: absolute;
+    right: 35px;
+    top:175px
+    
+}
+
+.rightbox{ 
+    position: absolute;
+    bottom: 24%;
+    left: 70%;
+    
+}
+
+.blackbox{
+    background-color:black;
+    width: 100%;
+    height: 15px;
+}
+
+.lastbox{
+    position: absolute;
+    /*top: 248%;
+    left:0%;*/
+    border: 1px solid;
+}
+
+
+
+
+
+@media screen and (min-width: 400px){
+    .box,.topcontainer,.one,.three,.flexbox1,.flex2,.flexbox2,.flex4,.container,.rightbox,.blackbox,.lastbox{
+        width: 100%;
+    }
+}
